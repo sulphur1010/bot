be3ff3658b2274cd97c0ba29c63ad8b8663533e9
Camilo Contreras <camilo.coo250@gmail.com>
2021-10-02 05:09:50 +0000

    tokkens addded

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 # Follow this guide if you need help: https://www.writebots.com/discord-bot-token/
 # Make sure to enable both privileged intents on the bot tab.
 # This is also how you will invite the bot to your server.
-token = "PLACE YOUR BOT TOKEN HERE"
+token = "ODkzNzIyNDE3OTUwNzY1MDg3.YVfl3A.aFJZ8RQXkY1EGf0DutE-VJAROaE"
 
 # Bot description shown in the help menu
 description = ""
@@ -10,13 +10,13 @@ description = ""
 # ID of the guild
 # To find this, go to your discord settings > advanced > enable developer mode
 # Then, right click the guild picture on the left sidebar and click copy ID
-guild_id = PLACE YOUR GUILD ID HERE
+guild_id = 893321767274303488
 
 # URLs of the OpenSea accounts that should be monitored and the channel IDs where their activity should be sent.
 # These are some examples. Feel free to add/remove as many as you wish but make sure to follow this template.
 # To find the IDs, go to your discord settings > advanced > enable developer mode
 # Then, right click the channel and click copy ID
-accounts_and_channels = [("https://opensea.io/GaryVee?tab=activity", PLACE CHANNEL ID HERE), ("https://opensea.io/pranksy?tab=activity", PLACE CHANNEL ID HERE)]
+accounts_and_channels = [("https://opensea.io/GaryVee?tab=activity", 893687009682554890), ("https://opensea.io/pranksy?tab=activity", 893687089395277895)]
 
 # Time (in minutes) between checking for new sales
 # All sales are always sent, no matter how often we check
